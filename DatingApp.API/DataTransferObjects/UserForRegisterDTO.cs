dc31876879458f8e052b12205837af1e67b88f70
EsTrevino <sttrevino2@gmail.com>
2019-01-04 11:44:07 -0700

    started AuthController, Register Method. created DTO folder and RegisterDTO

---
+++ DatingApp.API/DataTransferObjects/UserForRegisterDTO.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.DataTransferObjects
+{
+    public class UserForRegisterDTO
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
